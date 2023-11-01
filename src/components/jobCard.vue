b1ec1d29b8e6c72c69482229d5e0f58d65b6f8dc
Guocork <g1024536444@gmail.com>
2023-11-01 22:46:52 +0800

    优化首页ui，增添内容

--- src/components/jobCard.vue
+++ src/components/jobCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="job-list">
-    <div v-for="(job, index) in jobList" :key="index" 
+    <!-- <div v-for="(job, index) in jobList" :key="index" 
          class="job-item" 
          @mouseenter="showTooltip = true"
          @mouseleave="showTooltip = false" 
@@ -13,7 +13,24 @@
       <transition name="fade">
         <div v-if="showTooltip" class="tooltip">点击查看详情</div>
       </transition>
-    </div>
+    </div> -->
+    <el-card v-for="(job, index) in jobList" :key="index" class="box-card" 
+         @mouseenter="showTooltip = true"
+         @mouseleave="showTooltip = false" >
+      <template #header>
+        <div class="card-header">
+          <span>{{job.corporation }}</span>
+          <el-button class="button" @click="tiaozhuan1" text>点击投递</el-button>
+        </div>
+      </template>
+      <div class="text item">🚀  {{ job.title }}</div>
+      <div class="text item">👨‍🎓 {{ job.description }}</div>
+      <div class="text item">📍  {{ job.location }}</div>
+      <div class="text item">🏢 {{ job.education }}</div>
+    </el-card>
+    <!-- <transition name="fade">
+        <div v-if="showTooltip" class="tooltip">ggggggg</div>
+      </transition> -->
   </div>
 </template>
 
@@ -23,9 +40,11 @@ import { getMessage } from '@/api/methods'
 
 const jobList = ref([
   {
-    title:'java工程师',
-    description:'你要会Java',
-    location:'上海'
+    corporation:'青岛国信发展（集团）有限责任公司',
+    title: '技术开发岗（计算机方向）',
+    description: '1.具有计算机视觉、人工智能、物联网、大数据等技术研究与应用经历，熟练运用编程者优先；2.具有科研项目申报与管理相关经历；3.学习能力、理解能力、适应能力强，作风踏实过硬。',
+    location: '青岛市、烟台市、浙江省台州市，随船期间于青岛、浙江、福建、广东、海南等地',
+    education:'硕士|计算机科学与技术、软件工程、计算机应用技术、人工智能等。'
   }
 ]);
 const showTooltip = ref(false);
@@ -34,6 +53,10 @@ const fetchJobList = () => {
 
 };
 
+const tiaozhuan1=()=>{
+  window.open('https://xiaoyuan.zhaopin.com/job/CC234215610J40508363307', '_blank');
+}
+
 onMounted(() => {
   // 在组件加载时发送请求给后台，并将返回的职位信息存储在jobList中
   fetchJobList();
@@ -41,20 +64,23 @@ onMounted(() => {
 
 
 
-const navigateToJobPage = (url) => {
-  // 跳转到指定网页
-  window.open('https://colingo.ai/', '_blank');
-};
+// const navigateToJobPage = (url) => {
+//   // 跳转到指定网页
+//   window.open('https://colingo.ai/', '_blank');
+// };
+
+
+
 </script>
 
 <style scoped>
-.job-list {
+/* .job-list {
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
-}
+} */
 
-.job-item {
+/* .job-item {
   border: 1px solid #ccc;
   padding: 10px;
   border-radius: 5px;
@@ -63,27 +89,27 @@ const navigateToJobPage = (url) => {
   width: 200px;
   margin: 10px;
   position: relative;
-}
+} */
 
-.job-item:hover {
+.box-card:hover {
   background-color: #f0f0f0;
   transform: scale(1.02);
 }
 
-.job-title {
+/* .job-title {
   font-size: 20px;
   font-weight: bold;
-}
+} */
 
-.job-company {
+/* .job-company {
   font-size: 16px;
-}
+} */
 
-.job-description {
+/* .job-description {
   margin-top: 10px;
-}
+} */
 
-.tooltip {
+/* .tooltip {
   background-color: #333;
   color: #fff;
   padding: 5px 10px;
@@ -95,19 +121,40 @@ const navigateToJobPage = (url) => {
   opacity: 0.8;
   transition: opacity 0.3s, transform 0.3s;
   transform-origin: bottom;
-}
+} */
 
-.fade-enter-active,
+/* .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s, transform 0.3s;
-}
+} */
 
-.fade-enter,
+/* .fade-enter,
 .fade-leave-to
 
-/* .fade-leave-active in <2.1.8 */
-  {
+.fade-leave-active in <2.1.8 */
+  /* /* {
   opacity: 0;
   transform: translateY(-10px);
+} */
+
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
-</style>
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 400px;
+  margin-bottom: 20px;
+  border-radius: 10%;
+}
+</style> 
--- src/pages/youhua.vue
+++ src/pages/youhua.vue
@@ -1,35 +1,32 @@
 <template>
-    <el-row :gutter="10" class="hezi1">
-        <el-col :span="12">
-            <div class="grid-content ep-bg-purple">
-                    <el-input v-model="input1" placeholder="个人经历" style="height: 45px;" clearable
-                        :prefix-icon="Search" />
-            </div>
-        </el-col>
-        <el-col :span="6">
-            <div class="grid-content ep-bg-purple-light">        
-                    <el-button style="height: 45px;">生成简历</el-button>               
-            </div>
-        </el-col>
-        <el-col :span="6">
-            <div class="grid-content ep-bg-purple-light">        
-              <el-upload v-model:file-list="fileList" class="upload-demo"
-        action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" 
-        multiple :on-preview="handlePreview"
-        :on-remove="handleRemove" 
-        :before-remove="beforeRemove" 
-        :limit="3" :on-exceed="handleExceed">
-        <el-button  style="height: 45px;">上传简历</el-button>
-        <template #tip>
+  <el-row :gutter="10" class="hezi1">
+    <el-col :span="12">
+      <div class="grid-content ep-bg-purple">
+        <el-input v-model="input1" placeholder="个人经历" style="height: 45px;" clearable :prefix-icon="Search" />
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content ep-bg-purple-light">
+        <el-button style="height: 45px;">生成简历</el-button>
+      </div>
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content ep-bg-purple-light">
+        <el-upload v-model:file-list="fileList" class="upload-demo"
+          action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" multiple :on-preview="handlePreview"
+          :on-remove="handleRemove" :before-remove="beforeRemove" :limit="1" :on-exceed="handleExceed"
+          :on-success="handlesuccess">
+          <el-button style="height: 45px;">上传简历</el-button>
+          <template #tip>
             <!-- <div class="el-upload__tip">
                 文件大小不要超过500KB.
             </div> -->
-        </template>
-    </el-upload>          
-            </div>
-        </el-col>
-    </el-row>
-    <!-- <el-empty description="description" /> -->
+          </template>
+        </el-upload>
+      </div>
+    </el-col>
+  </el-row>
+  <!-- <el-empty description="description" /> -->
 </template>
 
 <script setup>
@@ -52,15 +49,19 @@ const handleRemove = (file, uploadFiles) => {
   console.log(file, uploadFiles)
 }
 
-const handlePreview= (uploadFile) => {
+const handlePreview = (uploadFile) => {
   console.log(uploadFile)
 }
 
-const handleExceed= (files, uploadFiles) => {
+const handlesuccess = (files, uploadFiles) => {
+  ElMessage.success(
+    '上传成功'
+  )
+}
+
+const handleExceed = (files, uploadFiles) => {
   ElMessage.warning(
-    `The limit is 3, you selected ${files.length} files this time, add up to ${
-      files.length + uploadFiles.length
-    } totally`
+    '只能上传一份简历'
   )
 }
 
@@ -75,7 +76,7 @@ const beforeRemove = (uploadFile, uploadFiles) => {
 </script>
 
 <style scoped>
-.hezi1{
+.hezi1 {
   margin-bottom: 20px;
 }
 </style>
