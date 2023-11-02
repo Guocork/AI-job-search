c8867716d8446b7134549935d94bab9494cdf7a3
lihao3 <lihao3@foxmail.com>
2023-11-02 22:30:12 +0800

    添加代码块，拆分请求

--- src/utils/service.js
+++ src/utils/service.js
@@ -6,12 +6,12 @@ import { ElMessage } from 'element-plus';
 // const NETWORK_ERROR = '网络请求异常，请稍后重试！';
 // 创建axios实例对象
 const service = axios.create({
-    timeout:100000,
-    baseURL:'http://124.222.10.135:9007',
-    headers:{
-        "Content-type":"application/json;charset=utf-8",
-        // "Authorization":"Bearer sk-a220ca3ea10d27ca0174c51e06a6dd00"
-    }
+  timeout: 100000,
+  baseURL: 'http://124.222.10.135:9007',
+  headers: {
+    "Content-type": "application/json;charset=utf-8",
+    // "Authorization":"Bearer sk-a220ca3ea10d27ca0174c51e06a6dd00"
+  }
 });
 
 // 在请求之前
@@ -23,14 +23,14 @@ service.interceptors.request.use((req) => {
 
 // 在请求之后
 service.interceptors.response.use((res) => {
-  const res1=res.data.run.results[0][0].value.content
-  const code= res.status;
-  //   根据后端协商 视情况而定
+  const res1 = res.data.run.results[0][0].value.content
+  const code = res.status;
+  // 根据后端协商 视情况而定
   if (code == 200) {
     return res1;
   } else if (code == 999) {
     ElMessage.error('网络请求失败，请稍后再试！');
-  } 
+  }
   // else {
   //   // 网络请求错误提示
   //   ElMessage.error(msg || NETWORK_ERROR);
@@ -44,18 +44,18 @@ function request(options) {
   if (options.method.toLowerCase() == 'get') {
     options.params = options.data;
   }
-//   //对mock的处理
-//   let isMock = config.isMock;
-//   if (typeof options.mock !== 'undefined') {
-//     isMock = options.mock;
-//   }
-//   // 对线上环境做处理
-//   if (config.env == 'prod') {
-//     // 不给你用到mock的机会
-//     service.defaults.baseURL = config.baseApi;
-//   } else {
-//     service.defaults.baseURL = isMock ? config.mockApi : config.baseApi;
-//   }
+  //   //对mock的处理
+  //   let isMock = config.isMock;
+  //   if (typeof options.mock !== 'undefined') {
+  //     isMock = options.mock;
+  //   }
+  //   // 对线上环境做处理
+  //   if (config.env == 'prod') {
+  //     // 不给你用到mock的机会
+  //     service.defaults.baseURL = config.baseApi;
+  //   } else {
+  //     service.defaults.baseURL = isMock ? config.mockApi : config.baseApi;
+  //   }
   return service(options);
 }
 
